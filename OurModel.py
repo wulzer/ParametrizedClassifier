04863bd91ed16c101cf66b3d056c5da115027e67
siyuchen95 <earthshinecsy@gmail.com>
2020-04-10 13:05:47 +0200

    L1Clip simplified.

--- OurModel.py
+++ OurModel.py
@@ -110,26 +110,28 @@ class OurModel(nn.Module):
     
     def ClipL1Norm(self):
 ### Clip the weights      
-        def ClipL1NormLayer(DesignatedL1Max, Layer):
+        def ClipL1NormLayer(DesignatedL1Max, Layer, Counter):
+            if Counter == 1:
+                ### this avoids clipping the first layer
+                return
             L1 = Layer.weight.abs().sum()
             Layer.weight.masked_scatter_(L1 > DesignatedL1Max, 
                                         Layer.weight*(DesignatedL1Max/L1))
+            return
         
         Counter = 0
         for m in self.children():
             if isinstance(m, nn.Linear):
                 Counter += 1
                 with torch.no_grad():
-                    DesignatedL1Max = self.L1MaxList[counter-1]
-                    if Counter != 1: ClipL1NormLayer(DesignatedL1Max, m)
-                    ### this avoids clipping the first layer
+                    DesignatedL1Max = m.weight.size(0)*m.weight.size(1)*self.L1perUnit
+                    ClipL1NormLayer(DesignatedL1Max, m, Counter)
             else:
                 for mm in m:
                     Counter +=1
                     with torch.no_grad():
-                        DesignatedL1Max = self.L1MaxList[counter-1]
-                        if Counter != 1: ClipL1NormLayer(DesignatedL1Max, mm)
-                        ### this avoids clipping the first layer
+                        DesignatedL1Max = mm.weight.size(0)*m.weight.size(1)*self.L1perUnit
+                        ClipL1NormLayer(DesignatedL1Max, mm, Counter)
         return 
     
     def DistributionRatio(self, points):
